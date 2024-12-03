78433e4d7436ced8a1c4acebbe4c9dce34ec30d8
dharithrirai <dharithrirai18@gmail.com>
2024-12-03 22:53:29 +0530

    added register page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
-import Login from './components/login';
-import Profile from './components/profile';
-// import Register from './components/Register'
+import Login from './components/Login';
+import Profile from './components/Profile';
+import Register from './components/Register'
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 // import Register from './components/Register'; // Adjust the path as needed
@@ -13,6 +13,8 @@ function App() {
             <div className="App">
                 <Routes>
                     <Route path="/login" element={<Login/>} />
+                    <Route path="/Register" element={<Register/>} />
+
                     {/* Add other routes here */}
                 </Routes>
             </div>
--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { registerUser } from './apiService'; // Adjust the path as needed
+// import { registerUser } from './apiService'; // Adjust the path as needed
+import '../Register.css'; 
 
 const Register = () => {
     const [formData, setFormData] = useState({
@@ -18,18 +19,19 @@ const Register = () => {
         });
     };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const response = await registerUser(formData);
-            setMessage(response.message);
-        } catch (error) {
-            setMessage(error.error);
-        }
-    };
+    // const handleSubmit = async (e) => {
+    //     e.preventDefault();
+    //     try {
+    //         const response = await registerUser(formData);
+    //         setMessage(response.message);
+    //     } catch (error) {
+    //         setMessage(error.error);
+    //     }
+    // };
 
     return (
-        <form onSubmit={handleSubmit}>
+        // <form onSubmit={handleSubmit}>
+        <form>
             <input type="text" name="username" value={formData.username} onChange={handleChange} placeholder="Username" required />
             <input type="email" name="email" value={formData.email} onChange={handleChange} placeholder="Email" required />
             <input type="password" name="password" value={formData.password} onChange={handleChange} placeholder="Password" required />
