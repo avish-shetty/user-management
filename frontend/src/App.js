48baa5be4acbdd4c1ee5ab4025205b73caf08100
dharithrirai <dharithrirai18@gmail.com>
2024-12-04 20:22:49 +0530

    modify register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import './App.css';
-import Login from './components/Login';
-import Profile from './components/Profile';
-import Register from './components/Register'
+import Login from './components/login';
+import Register from './components/register';
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 // import Register from './components/Register'; // Adjust the path as needed
 
 function App() {
@@ -13,7 +12,7 @@ function App() {
             <div className="App">
                 <Routes>
                     <Route path="/login" element={<Login/>} />
-                    <Route path="/Register" element={<Register/>} />
+                    <Route path="/register" element={<Register/>} />
                 </Routes>
             </div>
         </Router>
